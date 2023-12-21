326bc3952a1ee1871c2db6c60a1dc8cffc48f6f1
TOUZE <touzevirgile@gmail.com>
2023-12-21 18:19:04 +0100

    Clean #2

---
+++ SlimesCurse/Assets/Scripts/FadeTransitionMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTransitionMenu : MonoBehaviour
+{
+    [SerializeField] private float _fadeAnimationTime = 1.95f;
+    [SerializeField] private GameObject _fadeInCircle = null;
+
+    void Start()
+    {
+        _fadeInCircle.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (_fadeAnimationTime > 0)
+        {
+            _fadeAnimationTime -= Time.deltaTime;
+        }
+
+        if (_fadeAnimationTime <= 0)
+        {
+            _fadeInCircle.SetActive(false);
+        }
+    }
+}
